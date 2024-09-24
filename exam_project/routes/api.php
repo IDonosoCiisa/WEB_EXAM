277c943bbe11c5607add782ba5f20badffc0c29b
Ignacio Donoso <ignacio.donoso.salinas@ciisa.cl>
2024-09-24 00:26:33 -0300

    [IDONOSO]feature: security with apikey and middleware TODO Interface

--- exam_project/routes/api.php
+++ exam_project/routes/api.php
@@ -1,7 +1,8 @@
 <?php
 use App\Http\Controllers\UsuarioController;
+use App\Http\Middleware\ApiKeyMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('usuarios', UsuarioController::class);
-Route::apiResource('productos', ProductoController::class);
-Route::apiResource('clientes', ClienteController::class);
+Route::apiResource('usuarios', UsuarioController::class)->middleware(ApiKeyMiddleware::class);
+Route::apiResource('productos', ProductoController::class)->middleware(ApiKeyMiddleware::class);
+Route::apiResource('clientes', ClienteController::class)->middleware(ApiKeyMiddleware::class);
